cb310cfa6ed61d2d7f7e893dd2cbfba64a1b2c36
nanducosmic <nanducodes@gmail.com>
2025-09-25 14:06:44 +0530

    mainfirst commit"

---
+++ books.sql
@@ -0,0 +1,2 @@
+INSERT INTO bookstore VALUES (1, 'The Silent Patient', 'Alex Michaelides', 'Thriller', 399.00, 1200),(2, 'Atomic Habits', 'James Clear', 'Self-help', 499.00, 2000),(3, 'The Psychology of Money', 'Morgan Housel', 'Finance', 350.00, 1800);
+INSERT INTO bestseller VALUES(4, 'Ikigai', 'Francesc Miralles', 'Philosophy', 300.00, 2500),(5, 'Think Like a Monk', 'Jay Shetty', 'Self-help', 450.00, 2200)
