a8053de62fbb79a7bfe070e7d5f63fffaa733c11
nanducosmic <nanducodes@gmail.com>
2025-10-16 15:40:40 +0530

    Initial commit

---
+++ first.py
@@ -0,0 +1,25 @@
+price_Rice=45
+price_Sugar=40
+price_Oil=130
+
+customer_Rice=3
+customer_Sugar=2.5
+customer_Oil=1.8
+
+total_Rice=price_Rice*customer_Rice
+total_Sugar=price_Sugar*customer_Sugar
+total_Oil=price_Oil*customer_Oil
+
+sub_Total=total_Rice+total_Sugar+total_Oil
+
+delivery_charge = random.randint(5, 10)
+
+final_bill = subtotal + delivery_charge
+
+print("Grocerry Bill")
+print("Price for 3kg rice:"+str(total_Rice))
+print("Price for 2.5kg sugar:"+str(total_Sugar))
+print("Price for 1.8kg oil:"+int(total_Oil))
+print("Total price:"+int(sub_Total))
+print("Delivery charge:"+int(delivery_charge))
+print("Final Bill:"+int(final_bill))
---
+++ second.py
@@ -0,0 +1,14 @@
+paragraph = """
+Python is a powerful programming language. 
+This course helps beginners understand the basics of Python, 
+covering syntax, data types, and problem-solving techniques.
+"""
+length = len(paragraph)
+print("Length of the paragraph:", length)
+
+print("First character:", paragraph[0])
+print("Last character:", paragraph[-1])
+
+preview = paragraph[:50]
+print("\nPreview (first 50 characters):")
+print(preview)
