65b57f1482c74086eca8332f48133ac431fdc6ee
nanducosmic <nanducodes@gmail.com>
2025-10-17 15:36:29 +0530

    Initial commit

---
+++ third.py
@@ -0,0 +1,10 @@
+is_logged_in = True
+is_subscribed = False
+
+
+user_credits = 100
+max_credits = 200
+min_credits = 50
+
+print("is_logged_in:", is_logged_in)
+print("is_subscribed:", is_subscribed)
